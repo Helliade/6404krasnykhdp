6c530f9e7f4ebc5c433ffbda5d820d1cd30c60db
Helliade <dasha_krasnykh@mail.ru>
2025-12-21 16:14:21 +0400

    LR5_completed

--- ImageProcessing/tests/test_cat_image_processor.py
+++ ImageProcessing/tests/test_cat_image_processor.py
@@ -16,6 +16,25 @@ from ImageProcessing.implementation.api_processor import AsyncCatImageProcessor,
 class TestCatImageProcessorMinimal(unittest.TestCase):
     """Минимальные тесты, соответствующие заданию."""
     
+    def setUp(self):
+        """Подготовка общих тестовых данных."""
+        # Общие данные для тестов ImageData
+        self.test_image = np.zeros((50, 50, 3), dtype=np.uint8)
+        self.test_breed = "cat"
+        self.test_url = "test.jpg"
+        self.test_index = 1
+        
+        # Общие данные для тестов файлов
+        self.test_array_data = np.array([1, 2, 3])
+        
+        # Создаём ImageData для переиспользования
+        self.image_data = ImageData(
+            index=self.test_index,
+            breed=self.test_breed,
+            image_array=self.test_image,
+            url=self.test_url
+        )
+    
     def test_api_key_is_set(self):
         """Тест: API ключ устанавливается при инициализации."""
         with patch.dict(os.environ, {'API_KEY': 'test_key'}):
@@ -30,52 +49,41 @@ class TestCatImageProcessorMinimal(unittest.TestCase):
     
     def test_image_data_stores_breed(self):
         """Тест: ImageData корректно хранит породу."""
-        img = np.zeros((5, 5, 3), dtype=np.uint8)
-        data = ImageData(index=1, breed="cat", image_array=img, url="test.jpg")
-        self.assertEqual(data.breed, "cat")
+        self.assertEqual(self.image_data.breed, self.test_breed)
     
     def test_image_data_stores_index(self):
         """Тест: ImageData корректно хранит индекс."""
-        img = np.zeros((5, 5, 3), dtype=np.uint8)
-        data = ImageData(index=1, breed="cat", image_array=img, url="test.jpg")
-        self.assertEqual(data.index, 1)
+        self.assertEqual(self.image_data.index, self.test_index)
     
     def test_image_data_stores_url(self):
         """Тест: ImageData корректно хранит URL."""
-        img = np.zeros((5, 5, 3), dtype=np.uint8)
-        data = ImageData(index=1, breed="cat", image_array=img, url="test.jpg")
-        self.assertEqual(data.url, "test.jpg")
+        self.assertEqual(self.image_data.url, self.test_url)
     
     def test_image_data_stores_image_array(self):
         """Тест: ImageData корректно хранит массив изображения."""
-        img = np.zeros((5, 5, 3), dtype=np.uint8)
-        data = ImageData(index=1, breed="cat", image_array=img, url="test.jpg")
-        self.assertTrue(np.array_equal(data.image_array, img))
+        self.assertTrue(np.array_equal(self.image_data.image_array, self.test_image))
     
     def test_npy_file_can_be_written(self):
         """Тест: данные могут быть записаны в .npy файл."""
-        test_data = np.array([1, 2, 3])
         with tempfile.NamedTemporaryFile(suffix='.npy', delete=False) as tmp:
-            np.save(tmp.name, test_data)
+            np.save(tmp.name, self.test_array_data)
             self.assertTrue(os.path.exists(tmp.name))
         os.unlink(tmp.name)
     
     def test_npy_file_can_be_read(self):
         """Тест: данные могут быть прочитаны из .npy файла."""
-        test_data = np.array([1, 2, 3])
         with tempfile.NamedTemporaryFile(suffix='.npy', delete=False) as tmp:
-            np.save(tmp.name, test_data)
+            np.save(tmp.name, self.test_array_data)
             loaded = np.load(tmp.name)
-            self.assertTrue(np.array_equal(test_data, loaded))
+            self.assertTrue(np.array_equal(self.test_array_data, loaded))
         os.unlink(tmp.name)
     
     def test_file_write_and_read_preserves_data(self):
         """Тест: запись и чтение сохраняют данные."""
-        test_data = np.array([1, 2, 3])
         with tempfile.NamedTemporaryFile(suffix='.npy', delete=False) as tmp:
-            np.save(tmp.name, test_data)
+            np.save(tmp.name, self.test_array_data)
             loaded = np.load(tmp.name)
-            self.assertTrue(np.array_equal(test_data, loaded))
+            self.assertTrue(np.array_equal(self.test_array_data, loaded))
         os.unlink(tmp.name)
 
 if __name__ == '__main__':
